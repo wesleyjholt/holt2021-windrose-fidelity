9af4280937f1c2fca3ec817440c25bc51684c390
Wesley Holt <wesleyjholt@gmail.com>
2021-02-02 02:40:44 -0700

    add bash scripts to submit jobs to supercomputer

---
+++ bash-scripts/run_opt_9turb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=2048M  # memory per CPU core
+#SBATCH --time=00:00:30 # walltime
+#SBATCH -J 'Wind Rose Study. 9 turbs. alg: snopt.'
+#SBATCH --mail-user=wesleyjholt@gmail.com   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --array=1-10    # job array number corresponds to the layout numbers
+#SBATCH --qos=test
+
+ndirs = $1
+layout_number = ${SLURM_ARRAY_TASK_ID}
+echo "Optimizing layout ${layout_number} with ${ndirs} directions."
+
+module load julia
+cd ".."
+julia opt_9turb.jl ${layout_number} ${ndirs}
---
+++ bash-scripts/submit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for ndirs in 10 20 30 40 50 60 70 80 90 100 120 150 200 360
+do
+    sbatch submit.sh ${ndirs}
+    sleep 10
+done
