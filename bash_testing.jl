30f05bc6f9e6ca76feada4604c143a4c9cb5d0a7
Wesley Holt <wesleyjholt@gmail.com>
2021-02-04 11:47:55 -0700

    delete file used for testing on supercomputer

--- bash_testing.jl
+++
@@ -1,7 +0,0 @@
-layout_number = Base.parse(Int, ENV["layout_number"])
-ndirs = Base.parse(Int, ENV["ndirs"])
-
-println("layout number is: ", layout_number)
-println(typeof(layout_number), "\n")
-println("number of directions: ", ndirs)
-println(typeof(ndirs), "\n")
