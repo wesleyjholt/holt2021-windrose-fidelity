cd15f84a21f1c0291eab3252bc3de989a9d536fa
Wesley Holt <wesleyjholt@gmail.com>
2021-03-08 07:57:27 -0700

    add supercomputer results for 9-turbine circular farm

--- bash-scripts/run_opt_circle.sh
+++ bash-scripts/run_opt_circle.sh
@@ -2,17 +2,17 @@
 
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4096M  # memory per CPU core
-#SBATCH --time=01:20:00 # walltime
+#SBATCH --time=00:40:00 # walltime
 #SBATCH -J 'Wind Rose Study. 9 turbs. alg: snopt.'
 #SBATCH --mail-user=wesleyjholt@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
-#SBATCH --array=1-20    # job array number corresponds to the layout numbers
+#SBATCH --array=1-50    # job array number corresponds to the layout numbers
 # #SBATCH --qos=test
 
 ndirs=$1
 nturbines=9
-boundary_radius=450
+boundary_radius=1500
 layout_number=${SLURM_ARRAY_TASK_ID}
 echo "Optimizing layout ${layout_number} with ${ndirs} directions, ${nturbines} turbines in a ${boundary_radius}m-radius circular wind farm."
 
--- bash-scripts/submit.sh
+++ bash-scripts/submit.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
 for ndirs in 10 20 30 40 50 60 70 80 90 100 120 150 200 360
 do
-    for 
     sbatch run_opt_circle.sh ${ndirs}
     sleep 10
 done
