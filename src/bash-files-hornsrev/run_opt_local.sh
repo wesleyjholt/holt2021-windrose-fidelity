22aab29e64daacd77befd5895ae8c2a321d734c0
Wesley Holt <wesleyjholt@gmail.com>
2021-04-08 15:32:15 -0600

    move optimization functions to run_opt script

--- src/bash-files-hornsrev/run_opt_local.sh
+++ src/bash-files-hornsrev/run_opt_local.sh
@@ -4,8 +4,9 @@
 # THIS FILE RUNS A SINGLE OPTIMIZATION FOR THE SPECIFIED LAYOUT AND SET OF PARAMETERS.
 #######################################################################################
 
-# The layout number goes here.
-SLURM_ARRAY_TASK_ID=98
+# The number of cores and layout number goes here.
+export SLURM_NTASKS=1
+SLURM_ARRAY_TASK_ID=2
 
 # The values for these variables are pulled from the input arguments (from the "submit" file).
 layout_number=$(printf %3s $SLURM_ARRAY_TASK_ID | tr ' ' 0)
@@ -19,7 +20,7 @@ windrose=HornsRevWindRose
 turbine_type=VestasV80_2MW
 wake_model=GaussYawVariableSpread
 opt_algorithm=SnoptWECAlgorithm
-opt_algorithm_arg="1e-7, [3.0, 2.6, 2.2, 1.8, 1.4, 1.0, 1.0], [3.5e-1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-5], [false, false, false, false, false, false, true], Int(1e6), true"
+opt_algorithm_arg="1e-7, [3.0, 2.6, 2.2, 1.8, 1.4, 1.0, 1.0], [5e-1, 5e-1, 5e-1, 5e-1, 5e-1, 5e-1, 5e-1], [false, false, false, false, false, false, true], Int(1e2), true"
 boundary_shape=FreeFormBoundary
 
 # These are the file paths for the input and output info.
@@ -29,6 +30,23 @@ final_layout_file_name="final-layout-${layout_number}.yaml"
 opt_info_directory="../results/opt-info/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/layout-${layout_number}/"
 final_layout_figure_directory_path="../results/figures/final-layouts/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/"
 final_layout_figure_file_name="final-layout-${layout_number}.png"
+parallel_processing=true
 
 # Now, we run the optimization.
-julia run_opt.jl $ndirs $nspeeds $windrose $turbine_type $wake_model $opt_algorithm "$opt_algorithm_arg" $boundary_shape $boundary_input_arg $initial_layout_path $final_layout_directory_path $final_layout_file_name $opt_info_directory $final_layout_figure_directory_path $final_layout_figure_file_name
+julia run_opt.jl \
+$ndirs \
+$nspeeds \
+$windrose \
+$turbine_type \
+$wake_model \
+$opt_algorithm \
+"$opt_algorithm_arg" \
+$boundary_shape \
+$boundary_input_arg \
+$initial_layout_path \
+$final_layout_directory_path \
+$final_layout_file_name \
+$opt_info_directory \
+$final_layout_figure_directory_path \
+$final_layout_figure_file_name \
+$parallel_processing
--- src/run_opt.jl
+++ src/run_opt.jl
@@ -40,12 +40,15 @@ using DelimitedFiles
 using PyPlot
 import ForwardDiff
 
+ntasks = parse(Int, ENV["SLURM_NTASKS"])
 if _parallel_processing
-    addprocs(SlurmManager(parse(Int, ENV["SLURM_NTASKS"])-1))
-    @everywhere import FlowFarm; const ff=FlowFarm
-    @everywhere include.(["_aepmodel.jl", "_algorithm.jl", "_boundary.jl", "_farm.jl", "_layout.jl", "_opt.jl", "_plot.jl", "_turbine.jl", "_windrose.jl"])
+    if ntasks > 1
+        addprocs(SlurmManager(ntasks - 1))
+    end
+    @everywhere import FlowFarm; const ff = FlowFarm
+    include.(["_aepmodel.jl", "_algorithm.jl", "_boundary.jl", "_farm.jl", "_layout.jl", "_opt.jl", "_plot.jl", "_turbine.jl", "_windrose.jl"])
 else
-    import FlowFarm; const ff=FlowFarm
+    import FlowFarm; const ff = FlowFarm
     include.(["_aepmodel.jl", "_algorithm.jl", "_boundary.jl", "_farm.jl", "_layout.jl", "_opt.jl", "_plot.jl", "_turbine.jl", "_windrose.jl"])
 end
 
@@ -104,11 +107,155 @@ if _parallel_processing
     @everywhere farm_boundary = $farm_boundary
 end
 
+#################################################################################
+# DEFINE OPTIMIZATION FUNCTIONS
+#################################################################################
+
+if opt_algorithm.parallel_processing
+
+    # set up objective wrapper function
+    @everywhere function obj_with_TI(x, layout_param, model_param, opt_algorithm::SnoptWECAlgorithm)
+
+        # get number of turbines
+        nturbines = Int(length(x)/2)
+
+        # extract x and y locations of turbines from design variables vector
+        turbine_x = x[1:nturbines] 
+        turbine_y = x[nturbines+1:end]
+
+        # calculate AEP
+        AEP = opt_algorithm.objscale*ff.calculate_aep(turbine_x, turbine_y, layout_param.base_heights, model_param.turbine_design.rotor_diameter,
+                    model_param.turbine_design.hub_height, model_param.turbine_op.yaw, model_param.turbine_ct_models, model_param.turbine_design.generator_efficiency, model_param.turbine_design.cut_in_speed,
+                    model_param.turbine_design.cut_out_speed, model_param.turbine_design.rated_speed, model_param.turbine_design.rated_power, model_param.wind_resource_model, model_param.turbine_power_models, model_param.farm_model_with_ti,
+                    rotor_sample_points_y=model_param.velocity_sampling.rotor_sample_points_y, rotor_sample_points_z=model_param.velocity_sampling.rotor_sample_points_z)
+        
+        # return the objective as an array
+        return [AEP]
+    end
+
+    @everywhere function obj_no_TI(x, layout_param, model_param, opt_algorithm::SnoptWECAlgorithm)
+
+        # get number of turbines
+        nturbines = Int(length(x)/2)
+
+        # extract x and y locations of turbines from design variables vector
+        turbine_x = x[1:nturbines] 
+        turbine_y = x[nturbines+1:end]
+
+        # calculate AEP
+        AEP = opt_algorithm.objscale*ff.calculate_aep(turbine_x, turbine_y, layout_param.base_heights, model_param.turbine_design.rotor_diameter,
+                    model_param.turbine_design.hub_height, model_param.turbine_op.yaw, model_param.turbine_ct_models, model_param.turbine_design.generator_efficiency, model_param.turbine_design.cut_in_speed,
+                    model_param.turbine_design.cut_out_speed, model_param.turbine_design.rated_speed, model_param.turbine_design.rated_power, model_param.wind_resource_model, model_param.turbine_power_models, model_param.farm_model_no_ti,
+                    rotor_sample_points_y=model_param.velocity_sampling.rotor_sample_points_y, rotor_sample_points_z=model_param.velocity_sampling.rotor_sample_points_z)
+
+        # return the objective as an array
+        return [AEP]
+    end
+
+else
+
+    # set up objective wrapper function
+    function obj_with_TI(x)
+
+        # get number of turbines
+        nturbines = Int(length(x)/2)
+
+        # extract x and y locations of turbines from design variables vector
+        turbine_x = x[1:nturbines] 
+        turbine_y = x[nturbines+1:end]
+
+        # calculate AEP
+        AEP = opt_algorithm.objscale*ff.calculate_aep(turbine_x, turbine_y, layout_param.base_heights, model_param.turbine_design.rotor_diameter,
+                    model_param.turbine_design.hub_height, model_param.turbine_op.yaw, model_param.turbine_ct_models, model_param.turbine_design.generator_efficiency, model_param.turbine_design.cut_in_speed,
+                    model_param.turbine_design.cut_out_speed, model_param.turbine_design.rated_speed, model_param.turbine_design.rated_power, model_param.wind_resource_model, model_param.turbine_power_models, model_param.farm_model_with_ti,
+                    rotor_sample_points_y=model_param.velocity_sampling.rotor_sample_points_y, rotor_sample_points_z=model_param.velocity_sampling.rotor_sample_points_z)
+        
+        # return the objective as an array
+        return [AEP]
+    end
+
+    function obj_no_TI(x)
+
+        # get number of turbines
+        nturbines = Int(length(x)/2)
+
+        # extract x and y locations of turbines from design variables vector
+        turbine_x = x[1:nturbines] 
+        turbine_y = x[nturbines+1:end]
+
+        # calculate AEP
+        AEP = opt_algorithm.objscale*ff.calculate_aep(turbine_x, turbine_y, layout_param.base_heights, model_param.turbine_design.rotor_diameter,
+                    model_param.turbine_design.hub_height, model_param.turbine_op.yaw, model_param.turbine_ct_models, model_param.turbine_design.generator_efficiency, model_param.turbine_design.cut_in_speed,
+                    model_param.turbine_design.cut_out_speed, model_param.turbine_design.rated_speed, model_param.turbine_design.rated_power, model_param.wind_resource_model, model_param.turbine_power_models, model_param.farm_model_no_ti,
+                    rotor_sample_points_y=model_param.velocity_sampling.rotor_sample_points_y, rotor_sample_points_z=model_param.velocity_sampling.rotor_sample_points_z)
+
+        # return the objective as an array
+        return [AEP]
+    end
+
+end
+
+obj_with_TI(x) = obj_with_TI(x, layout_param, model_param, opt_algorithm)
+obj_no_TI(x) = obj_no_TI(x, layout_param, model_param, opt_algorithm)
+
+# set up constraint wrapper function
+function con(x, model_param, boundary)
+
+    # get number of turbines
+    nturbines = Int(length(x)/2)
+    
+    # extract x and y locations of turbines from design variables vector
+    turbine_x = x[1:nturbines]
+    turbine_y = x[nturbines+1:end]
+
+    # get constraint values
+    spacing_con = 2.0*model_param.turbine_design.rotor_diameter[1] .- ff.turbine_spacing(turbine_x,turbine_y)
+    boundary_con = ff.ray_trace_boundary(boundary.vertices, boundary.normals, turbine_x, turbine_y)
+    
+    return [spacing_con; boundary_con]
+end
+
+con(x) = con(x, model_param, farm_boundary)
+
+# set up optimization problem wrapper function
+function wind_farm_opt_with_TI(x)
+
+    # calculate the objective and jacobian (negative sign in order to maximize AEP)
+    AEP = -obj_with_TI(x)[1]
+    dAEP_dx = -ForwardDiff.jacobian(obj_with_TI, x)
+
+    # calculate constraint and jacobian
+    c = con(x)
+    dc_dx = ForwardDiff.jacobian(con, x)
+
+    # set fail flag to false
+    fail = false
+
+    # return objective, constraint, and jacobian values
+    return AEP, c, dAEP_dx, dc_dx, fail
+end
+
+function wind_farm_opt_no_TI(x)
+
+    # calculate the objective and jacobian (negative sign in order to maximize AEP)
+    AEP = -obj_no_TI(x)[1]
+    dAEP_dx = -ForwardDiff.jacobian(obj_no_TI, x)
+
+    # calculate constraint and jacobian
+    c = con(x)
+    dc_dx = ForwardDiff.jacobian(con, x)
+
+    # set fail flag to false
+    fail = false
+
+    # return objective, constraint, and jacobian values
+    return AEP, c, dAEP_dx, dc_dx, fail
+end
 
 #################################################################################
 # RUN OPTIMIZATION
 #################################################################################
-turbine_x_opt, turbine_y_opt = optimize_farm_layout(final_layout_path, _opt_info_directory, layout_param, model_param, opt_algorithm, farm_boundary)
+turbine_x_opt, turbine_y_opt = optimize_farm_layout(wind_farm_opt_with_TI, wind_farm_opt_no_TI, final_layout_path, _opt_info_directory, layout_param, model_param, opt_algorithm, farm_boundary)
 
 #################################################################################
 # PLOT LAYOUT
