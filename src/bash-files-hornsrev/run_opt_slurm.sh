8b1c46241be3b84235a1c7fb34b8f2dd108ebd5c
Wesley Holt <wesleyjholt@gmail.com>
2021-04-09 01:36:32 -0600

    change job array

--- src/bash-files-hornsrev/run_opt_slurm.sh
+++ src/bash-files-hornsrev/run_opt_slurm.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 #SBATCH --mem-per-cpu=4096M  # memory per CPU core
-#SBATCH --time=02:00:00 # walltime
+#SBATCH --time=04:00:00 # walltime
 #SBATCH -J 'Wind Rose Study, Horns Rev 1 Wind Farm'
 # #SBATCH --mail-user=wesleyjholt@gmail.com   # email address
 # #SBATCH --mail-type=BEGIN
 # #SBATCH --mail-type=END
-#SBATCH --array=11-15    # job array number corresponds to the layout numbers
+#SBATCH --array=16-50    # job array number corresponds to the layout numbers
 # #SBATCH --qos=test
 
 
@@ -27,10 +27,10 @@ boundary_shape=FreeFormBoundary
 
 # These are the file paths for the input and output info.
 initial_layout_path="../data/initial-layouts/horns-rev/initial-layout-${layout_number}.txt"
-final_layout_directory_path="../results/final-layouts/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/"
+final_layout_directory_path="../results/final-layouts/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
 final_layout_file_name="final-layout-${layout_number}.yaml"
-opt_info_directory="../results/opt-info/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/layout-${layout_number}/"
-final_layout_figure_directory_path="../results/figures/final-layouts/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/"
+opt_info_directory="../results/opt-info/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/layout-${layout_number}/"
+final_layout_figure_directory_path="../results/figures/final-layouts/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
 final_layout_figure_file_name="final-layout-${layout_number}.png"
 parallel_processing=true
 
