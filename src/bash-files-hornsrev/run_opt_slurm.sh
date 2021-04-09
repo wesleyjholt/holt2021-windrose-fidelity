9171623f037be45b2a7bfaf07aaa7cd1378996b2
Wesley Holt <wesleyjholt@gmail.com>
2021-04-09 02:20:58 -0600

    change job array

--- src/bash-files-hornsrev/run_opt_slurm.sh
+++ src/bash-files-hornsrev/run_opt_slurm.sh
@@ -6,7 +6,7 @@
 # #SBATCH --mail-user=wesleyjholt@gmail.com   # email address
 # #SBATCH --mail-type=BEGIN
 # #SBATCH --mail-type=END
-#SBATCH --array=16-30    # job array number corresponds to the layout numbers
+#SBATCH --array=31-50    # job array number corresponds to the layout numbers
 # #SBATCH --qos=test
 
 
