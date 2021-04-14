7a571a6679f9a6cf6ce0a94e0bf2446156be7215
Wesley Holt <wesleyjholt@gmail.com>
2021-04-14 03:33:33 -0600

    changes to bash files for circle opt

--- src/bash-files-circle/run_opt_slurm.sh
+++ src/bash-files-circle/run_opt_slurm.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #SBATCH --mem-per-cpu=4096M  # memory per CPU core
-#SBATCH --time=02:00:00 # walltime
+#SBATCH --time=00:05:00 # walltime
 #SBATCH -J 'Wind Rose Study, Horns Rev 1 Wind Farm'
 #SBATCH -o ./Report/output.%a.out
-#SBATCH --array=3-5    # job array number corresponds to the layout numbers
-# #SBATCH --qos=test
+#SBATCH --array=1-2    # job array number corresponds to the layout numbers
+#SBATCH --qos=test
 
 
 # The values for these variables are pulled from the input arguments (from the "submit" file).
@@ -24,30 +24,30 @@ opt_algorithm_arg="1e-6, [3.0, 2.6, 2.2, 1.8, 1.4, 1.0, 1.0], [3e-3, 3e-3, 1e-3,
 boundary_shape=FreeFormBoundary
 
 # These are the file paths for the input and output info.
-initial_layout_path="../data/initial-layouts/horns-rev/initial-layout-${layout_number}.txt"
-final_layout_directory_path="../results/final-layouts/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
+initial_layout_path="../data/initial-layouts/circle/${nturbines}turb/${boundary_radius}r/initial-layout-${layout_number}.txt"
+final_layout_directory_path="../results/final-layouts/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
 final_layout_file_name="final-layout-${layout_number}.yaml"
-opt_info_directory="../results/opt-info/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/layout-${layout_number}/"
-final_layout_figure_directory_path="../results/figures/final-layouts/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
+opt_info_directory="../results/opt-info/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/layout-${layout_number}/"
+final_layout_figure_directory_path="../results/figures/final-layouts/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
 final_layout_figure_file_name="final-layout-${layout_number}.png"
 parallel_processing=true
 
 # Now, we run the optimization.
 module load julia
 julia run_opt.jl \
-$ndirs \
-$nspeeds \
-$windrose \
-$turbine_type \
-$wake_model \
-$opt_algorithm \
-"$opt_algorithm_arg" \
-$boundary_shape \
-$boundary_input_arg \
-$initial_layout_path \
-$final_layout_directory_path \
-$final_layout_file_name \
-$opt_info_directory \
-$final_layout_figure_directory_path \
-$final_layout_figure_file_name \
-$parallel_processing
+    $ndirs \
+    $nspeeds \
+    $windrose \
+    $turbine_type \
+    $wake_model \
+    $opt_algorithm \
+    "$opt_algorithm_arg" \
+    $boundary_shape \
+    $boundary_input_arg \
+    $initial_layout_path \
+    $final_layout_directory_path \
+    $final_layout_file_name \
+    $opt_info_directory \
+    $final_layout_figure_directory_path \
+    $final_layout_figure_file_name \
+    $parallel_processing
--- src/bash-files-circle/submit_circle.sh
+++ src/bash-files-circle/submit_circle.sh
@@ -23,22 +23,28 @@
 # This file assumes that user is running it from the "src" directory.
 
 # set variable values
-nturbines=80
-boundary_file_path="../data/input-files/horns-rev-boundary.txt"
-ndirs_vec=(12 360) #(12 36 72 120 360)
-nspeeds_vec=(1 25) #(1 2 5 10 25)
+nturbines_vec=(9) #(9 18)
+boundary_radius_vec=(900) #(450 600 900 1200 1500)
+ndirs_vec=(360 180 120 90 72 60 51 45 36 33 30 28 26 24 22 20 18 16 14 12 10 8)
+nspeeds_vec=(1 5 10 20)
 ntasksmax=250
 
 # run optimizations for each combination of variables
-for ndirs in ${ndirs_vec[@]}
+for nturbines in ${nturbines_vec[@]}
 do
-    for nspeeds in ${nspeeds_vec[@]}
+    for boundary_radius in ${boundary_radius_vec[@]}
     do
-        ntasksdefault=$(expr $ndirs \* $nspeeds / 500)
-        _ntasks=$(($ntasksdefault<$ntasksmax ? $ntasksdefault : $ntasksmax))
-        _ntasks=$(($_ntasks>1 ? $_ntasks : 1))
-        echo "Now submitting jobs for $ndirs directions and $nspeeds speeds, using $_ntasks CPUs."
-        sbatch --ntasks=$_ntasks bash-files-circle/run_opt_slurm.sh $nturbines $boundary_file_path $ndirs $nspeeds
-        sleep 3
+        for ndirs in ${ndirs_vec[@]}
+        do
+            for nspeeds in ${nspeeds_vec[@]}
+            do
+                ntasksdefault=$(expr $ndirs \* $nspeeds / 500)
+                _ntasks=$(($ntasksdefault<$ntasksmax ? $ntasksdefault : $ntasksmax))
+                _ntasks=$(($_ntasks>1 ? $_ntasks : 1))
+                echo "Now submitting jobs for $ndirs directions and $nspeeds speeds, using $_ntasks CPUs."
+                sbatch --ntasks=$_ntasks bash-files-circle/run_opt_slurm.sh $nturbines $boundary_radius $ndirs $nspeeds
+                sleep 1
+            done
+        done
     done
 done
