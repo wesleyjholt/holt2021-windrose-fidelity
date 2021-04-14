e8442ef18fb1501ea47af086f802ef3fa9215062
Wesley Holt <wesleyjholt@gmail.com>
2021-04-14 16:56:11 -0600

    add parallel processing capability to AEP post processing calculator

--- src/bash-files-circle/postprocess_circle.sh
+++ src/bash-files-circle/postprocess_circle.sh
@@ -1,24 +1,32 @@
 # CALCULATE AEPS WITH SEVERAL DIFFERENT WIND ROSE FIDELITY LEVELS AND WAKE MODELS
 
+export SLURM_NTASKS=1
+
+# set what analyses to run
+calculate_aeps=true
+plot_layouts=false
+plot_aeps=false
+plot_montecarlo=false
+
 # set model parameters
 nturbines_vec=(9)
-boundary_radius_vec=(900) #(450 600 900 1200 1500)
-ndirs_vec=(10 20 30 40 50 60 70 80 90 100 120 150 200 360)
-ndirs_vec_julia=[10,20,30,40,50,60,70,80,90,100,120,150,200,360]
-nspeeds_vec=(1)
-nspeeds_vec_julia_directory="1"
-# for i in $(seq 1 ${#nspeeds_vec_julia})
-windrose=NantucketWindRose
-turbine_type=NREL_5MW
-wake_model_vec=(GaussYawVariableSpread) #JensenCosine)
+boundary_radius_vec=(600 900 1200)
+ndirs_vec=(360 180) #(360 180 120 90 72 60 51 45 36 33 30 28 26 24 22 20 18 16 14 12 10 8)
+ndirs_vec_julia=[360,180] #[360,180,120,90,72,60,51,45,36,33,30,28,26,24,22,20,18,16,14,12,10,8]
+nspeeds_vec=(1 10) #(1 5 10 20)
+nspeeds_vec_julia=[01,10] #[01,05,10,20]
+windrose=HornsRevWindRose
+turbine_type=VestasV80_2MW
+wake_model_vec=(GaussYawVariableSpread)
 opt_algorithm=SnoptWECAlgorithm
 
 # set layout numbers
 layout_number_start=1
-layout_number_end=50
+layout_number_end=2
 
-# figure parameters
-aeps_fig_type=ConfidenceIntervalPlot
+# AEP plot parameters
+aeps_fig_type=ConfidenceIntervalScatterPlot
+
 
 # calculate and plot all AEP values for the specified layouts
 for nturbines in ${nturbines_vec[@]}                                    # number of turbines
@@ -37,28 +45,198 @@ do
 
                     for nspeeds in ${nspeeds_vec[@]}                    # number of wind speeds used for optimization
                     do
-                        nspeeds=$(printf %3s $nspeeds | tr ' ' 0)
+                        nspeeds=$(printf %2s $nspeeds | tr ' ' 0)
 
-                        # calculate AEP values
-                        layouts_directory_path="../results/final-layouts/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/"
-                        aeps_directory_path="../results/final-aeps/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/"
-                        aeps_file_path=$aeps_directory_path$aeps_file_name
-                        julia calc_aeps.jl 360 1 $windrose $nturbines $turbine_type $analysis_wake_model $layout_number_start $layout_number_end $layouts_directory_path $aeps_directory_path $aeps_file_name  
+                        if $calculate_aeps == true
+                        then
+                            # calculate AEP values
+                            layouts_directory_path="../results/final-layouts/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
+                            aeps_directory_path="../results/final-aeps/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
+                            aeps_file_path=$aeps_directory_path$aeps_file_name
+                            parallel_processing=true
+                            julia calc_aeps.jl \
+                                360 \
+                                1 \
+                                $windrose \
+                                $nturbines \
+                                $turbine_type \
+                                $analysis_wake_model \
+                                $layout_number_start \
+                                $layout_number_end \
+                                $layouts_directory_path \
+                                $aeps_directory_path \
+                                $aeps_file_name \
+                                $parallel_processing
+                        fi
                     done
 
-                        # # plot all layouts overlayed on each other for a specific direction-speed fidelity combination
-                        # layout_plots_directory_path="../results/figures/final-layouts/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
-                        # layout_plots_file_name="final-layouts-plot.png"
-                        # julia plot_overlayed_layouts_by_direction_speed.jl $layout_number_start $layout_number_end $layouts_directory_path $layout_plots_directory_path $layout_plots_file_name
+                    if $plot_layouts == true
+                    then
+                        # plot all layouts overlayed on each other for a specific direction-speed fidelity combination
+                        layout_plots_directory_path="../results/figures/final-layouts/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
+                        layout_plots_file_name="final-layouts-plot.png"
+                        julia plot_overlayed_layouts_by_direction_speed.jl $layout_number_start $layout_number_end $layouts_directory_path $layout_plots_directory_path $layout_plots_file_name
+                    fi
                 done
 
-                # plot AEP values
-                aeps_directory_partial_path="../results/final-aeps/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/"
-                aeps_plot_directory_path="../results/figures/aep-plots/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/"
-                aeps_plot_file_name="final-aeps-${analysis_wake_model}-plot.png"
-                aeps_fig_title="AEP Values for Optimized, $nturbines-turbine, Circular Wind Farm Layouts"
-                julia plot_aeps.jl $ndirs_vec_julia $nspeeds_vec_julia_directory $layout_number_start $layout_number_end $aeps_directory_partial_path $aeps_file_name $aeps_plot_directory_path $aeps_plot_file_name "$aeps_fig_title" $aeps_fig_type
+                if $plot_aeps == true
+                then
+                    # plot AEP values
+                    aeps_directory_partial_path="../results/final-aeps/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/"
+                    aeps_plot_directory_path="../results/figures/aep-plots/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/"
+                    aeps_plot_file_name="final-aeps-${analysis_wake_model}-plot.png"
+                    aeps_fig_title="AEP Values for Optimized, $nturbines-turbine, Circular Wind Farm Layouts"
+                    julia plot_aeps.jl \
+                        $ndirs_vec_julia \
+                        $nspeeds_vec_julia \
+                        $layout_number_start \
+                        $layout_number_end \
+                        $aeps_directory_partial_path \
+                        $aeps_file_name \
+                        $aeps_plot_directory_path \
+                        $aeps_plot_file_name \
+                        "$aeps_fig_title" \
+                        $aeps_fig_type
+                fi
+                
+                if $plot_montecarlo == true
+                then
+                    # plot Monte Carlo results
+                    aeps_directory_partial_path="../results/final-aeps/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/"
+                    montecarlo_plot_directory_path="../results/figures/aep-plots/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/reanalyzed-with-${analysis_wake_model}/"
+                    aeps_fig_title=""
+                    julia plot_montecarlo.jl \
+                        $ndirs_vec_julia \
+                        $nspeeds_vec_julia \
+                        $analysis_wake_model \
+                        $layout_number_start \
+                        $layout_number_end \
+                        $aeps_directory_partial_path \
+                        $aeps_file_name \
+                        $montecarlo_plot_directory_path
+                fi 
             done
         done
     done
 done
+
+
+
+
+
+# CALCULATE AEPS WITH SEVERAL DIFFERENT WIND ROSE FIDELITY LEVELS AND WAKE MODELS
+
+# set what analyses to run
+calculate_aeps=true
+plot_layouts=false
+plot_aeps=false
+plot_montecarlo=false
+
+# set model parameters
+nturbines_vec=(80)
+ndirs_vec=(12 36 72 120 360)
+ndirs_vec_julia=[12,36,72,120,360]
+nspeeds_vec=(1 5 10 15 20 25)
+nspeeds_vec_julia=[01,05,10,15,20,25]
+windrose=HornsRevWindRose
+turbine_type=VestasV80_2MW
+wake_model_vec=(JensenCosine) #(GaussYawVariableSpread)
+opt_algorithm=SnoptWECAlgorithm
+
+# set layout numbers
+layout_number_start=1
+layout_number_end=20
+
+# AEP plot parameters
+aeps_fig_type=ConfidenceIntervalScatterPlot
+
+# Monte Carlo plot parameters
+sample_sizes=[]
+
+# calculate and plot all AEP values for the specified layouts
+for nturbines in ${nturbines_vec[@]}                                    # number of turbines
+do
+    for wake_model in ${wake_model_vec[@]}                          # optimization wake model
+    do
+        for analysis_wake_model in JensenCosine GaussYawVariableSpread #${wake_model_vec[@]}             # analysis wake model
+        do
+            aeps_file_name="final-aeps-${analysis_wake_model}.txt"
+
+            for ndirs in ${ndirs_vec[@]}                            # number of wind directions used for optimization
+            do
+                ndirs=$(printf %3s $ndirs | tr ' ' 0)
+
+                for nspeeds in ${nspeeds_vec[@]}                    # number of wind speeds used for optimization
+                do
+                    nspeeds=$(printf %2s $nspeeds | tr ' ' 0)
+
+                    if $calculate_aeps == true
+                    then
+                        # calculate AEP values
+                        layouts_directory_path="../results/final-layouts/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
+                        aeps_directory_path="../results/final-aeps/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
+                        aeps_file_path=$aeps_directory_path$aeps_file_name
+                        julia calc_aeps.jl \
+                            360 \
+                            25 \
+                            $windrose \
+                            $nturbines \
+                            $turbine_type \
+                            $analysis_wake_model \
+                            $layout_number_start \
+                            $layout_number_end \
+                            $layouts_directory_path \
+                            $aeps_directory_path \
+                            $aeps_file_name
+                    fi
+                done
+
+                if $plot_layouts == true
+                then
+                    # plot all layouts overlayed on each other for a specific direction-speed fidelity combination
+                    layout_plots_directory_path="../results/figures/final-layouts/circle/${nturbines}turb/${boundary_radius}r/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/${ndirs}dirs/${nspeeds}speeds/"
+                    layout_plots_file_name="final-layouts-plot.png"
+                    julia plot_overlayed_layouts_by_direction_speed.jl $layout_number_start $layout_number_end $layouts_directory_path $layout_plots_directory_path $layout_plots_file_name
+                fi
+            done
+
+            if $plot_aeps == true
+            then
+                # plot AEP values
+                aeps_directory_partial_path="../results/final-aeps/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/"
+                aeps_plot_directory_path="../results/figures/aep-plots/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/reanalyzed-with-${analysis_wake_model}/"
+                aeps_fig_title="AEP Values for Optimized, $nturbines-turbine, Horns Rev Wind Farm Layouts"
+                julia plot_aeps.jl \
+                    $ndirs_vec_julia \
+                    $nspeeds_vec_julia \
+                    $analysis_wake_model \
+                    $layout_number_start \
+                    $layout_number_end \
+                    $aeps_directory_partial_path \
+                    $aeps_file_name \
+                    $aeps_plot_directory_path \
+                    "$aeps_fig_title" \
+                    $aeps_fig_type
+            fi
+
+            if $plot_montecarlo == true
+            then
+                # plot Monte Carlo results
+                aeps_directory_partial_path="../results/final-aeps/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/"
+                montecarlo_plot_directory_path="../results/figures/aep-plots/horns-rev/${windrose}/${turbine_type}/${wake_model}/${opt_algorithm}/reanalyzed-with-${analysis_wake_model}/"
+                aeps_fig_title=""
+                julia plot_montecarlo.jl \
+                    $ndirs_vec_julia \
+                    $nspeeds_vec_julia \
+                    $analysis_wake_model \
+                    $layout_number_start \
+                    $layout_number_end \
+                    $aeps_directory_partial_path \
+                    $aeps_file_name \
+                    $montecarlo_plot_directory_path
+            fi 
+        done
+    done
+done
+
+
--- src/calc_aeps.jl
+++ src/calc_aeps.jl
@@ -1,7 +1,3 @@
-include.(["_aepmodel.jl", "_farm.jl", "_layout.jl", "_turbine.jl","_windrose.jl"])
-import FlowFarm; const ff=FlowFarm
-using DelimitedFiles
-
 #################################################################################
 # GET INPUT ARGUMENTS
 #################################################################################
@@ -16,8 +12,33 @@ _layout_number_end = parse(Int64, ARGS[8])
 _layouts_directory_path = ARGS[9]
 _aeps_directory_path = ARGS[10]
 _aeps_file_name = ARGS[11]
+_parallel_processing = parse(Bool, ARGS[12])
 
 layout_number_vec = _layout_number_start:_layout_number_end
+
+
+#################################################################################
+# IMPORT PACKAGES
+#################################################################################
+using DelimitedFiles
+using Distributed
+using ClusterManagers
+
+ntasks = parse(Int, ENV["SLURM_NTASKS"])
+println("Using $ntasks processors.")
+if _parallel_processing
+    if ntasks > 1
+        addprocs(SlurmManager(ntasks - 1))
+        println("\nProcessors have been added.")
+    end
+    @everywhere import FlowFarm; const ff = FlowFarm
+    @everywhere include.(["_aepmodel.jl", "_farm.jl", "_layout.jl", "_turbine.jl","_windrose.jl"])
+else
+    import FlowFarm; const ff = FlowFarm
+    include.(["_aepmodel.jl", "_farm.jl", "_layout.jl", "_turbine.jl","_windrose.jl"])
+end
+println("Packages are imported.")
+
 
 #################################################################################
 # SET LAYOUT AND MODEL PARAMETERS, CALCULATE AEPS
@@ -26,32 +47,77 @@ layout_number_vec = _layout_number_start:_layout_number_end
 model_param = model_set(_wake_model, _turbine_type, _nturbines, _windrose, _ndirs, _nspeeds)
 
 # iterate through each layout
+println("\nNow calculating AEPs for final layouts $(layout_number_vec[1]) and $(layout_number_vec[end]).")
 nlayouts = length(layout_number_vec)
 aeps = zeros(nlayouts)
+good_indices = []
+nan_indices = []
 for i = 1:nlayouts
     # get layout number
     layout_number = layout_number_vec[i]
 
     # get turbine layout
-    layouts_file_path = _layouts_directory_path * "final-layout-$(lpad(layout_number,3,"0")).yaml"
-    layout_param = layout_set(layouts_file_path, file_type="yaml")
+    if isfile(_layouts_directory_path * "final-layout-$(lpad(layout_number,3,"0")).yaml") 
+        layouts_file_path = _layouts_directory_path * "final-layout-$(lpad(layout_number,3,"0")).yaml"
+        layout_param = layout_set(layouts_file_path, file_type="yaml")
 
-    # calculate AEP
-    aeps[i] = ff.calculate_aep(layout_param.turbine_x, layout_param.turbine_y, layout_param.base_heights, model_param.turbine_design.rotor_diameter,
-                    model_param.turbine_design.hub_height, model_param.turbine_op.yaw, model_param.turbine_ct_models, model_param.turbine_design.generator_efficiency, model_param.turbine_design.cut_in_speed,
-                    model_param.turbine_design.cut_out_speed, model_param.turbine_design.rated_speed, model_param.turbine_design.rated_power, model_param.wind_resource_model, model_param.turbine_power_models, model_param.farm_model_with_ti,
-                    rotor_sample_points_y=model_param.velocity_sampling.rotor_sample_points_y, rotor_sample_points_z=model_param.velocity_sampling.rotor_sample_points_z)
+        if _parallel_processing
+            @everywhere layout_param = $layout_param
+            @everywhere model_param = $model_param
+            @everywhere calculate_aeps_parallel = ff.calculate_aep
+            # calculate AEP
+            aeps[i] = calculate_aeps_parallel(layout_param.turbine_x, layout_param.turbine_y, layout_param.base_heights, model_param.turbine_design.rotor_diameter,
+                            model_param.turbine_design.hub_height, model_param.turbine_op.yaw, model_param.turbine_ct_models, model_param.turbine_design.generator_efficiency, model_param.turbine_design.cut_in_speed,
+                            model_param.turbine_design.cut_out_speed, model_param.turbine_design.rated_speed, model_param.turbine_design.rated_power, model_param.wind_resource_model, model_param.turbine_power_models, model_param.farm_model_with_ti,
+                            rotor_sample_points_y=model_param.velocity_sampling.rotor_sample_points_y, rotor_sample_points_z=model_param.velocity_sampling.rotor_sample_points_z)
+        else
+            # calculate AEP
+            aeps[i] = ff.calculate_aep(layout_param.turbine_x, layout_param.turbine_y, layout_param.base_heights, model_param.turbine_design.rotor_diameter,
+                            model_param.turbine_design.hub_height, model_param.turbine_op.yaw, model_param.turbine_ct_models, model_param.turbine_design.generator_efficiency, model_param.turbine_design.cut_in_speed,
+                            model_param.turbine_design.cut_out_speed, model_param.turbine_design.rated_speed, model_param.turbine_design.rated_power, model_param.wind_resource_model, model_param.turbine_power_models, model_param.farm_model_with_ti,
+                            rotor_sample_points_y=model_param.velocity_sampling.rotor_sample_points_y, rotor_sample_points_z=model_param.velocity_sampling.rotor_sample_points_z)
+        end
+        append!(good_indices, i)
+    else
+        aeps[i] = NaN
+        append!(nan_indices, i)
+    end
+end
+
+#################################################################################
+# GET THE MISSING LAYOUT NUMBERS
+#################################################################################
+good_layouts = good_indices .+ _layout_number_start .- 1
+nan_layouts = nan_indices .+ _layout_number_start .- 1
+
+println("There are $(length(nan_indices)) missing final layouts.")
+if length(nan_indices) > 0
+    println("Missing layout numbers are: $(nan_layouts)")
 end
 
 
 #################################################################################
-# SAVE AEP VALUES TO A TEXT FILE
+# SAVE ALL AEP VALUES TO A TEXT FILE
 #################################################################################
 # create intermediate directories (if they don't already exist)
 mkpath(_aeps_directory_path)
 
 # write to text file
 open(_aeps_directory_path * _aeps_file_name, "w") do io
-    write(io, "# AEP values for layouts $_layout_number_start to $_layout_number_end\n")
-    writedlm(io, aeps)
+    write(io, "# layout number, AEP\n")
+    for i = 1:length(layout_number_vec)
+        write(io, "$(lpad(Int(layout_number_vec[i]),3,"0"))\t$(aeps[i])\n")
+    end
+end
+
+
+#################################################################################
+# SAVE CLEANED AEP VALUES TO A TEXT FILE (NO NANs)
+#################################################################################
+# write to text file
+open(_aeps_directory_path * "cleaned-" * _aeps_file_name, "w") do io
+    write(io, "# layout number, AEP (with NaN values removed)\n")
+    for i = 1:length(good_layouts)
+        write(io, "$(lpad(Int(good_layouts[i]),3,"0"))\t$(aeps[good_indices][i])\n")
+    end
 end
